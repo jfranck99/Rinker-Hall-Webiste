741d54466fd0c7f7b3bc6f7cf1d801663149ec35
Jon Franck <rexdude99@gmail.com>
2021-05-12 13:35:14 -0400

    Add files via upload

---
+++ scripts/rinkerFunctions.js
@@ -0,0 +1,12 @@
+/* Jonathan Franck
+11/23/18
+Rinker Hall Functions List
+*/
+//Thank you message
+function tyAlert() {
+     alert("Thank you for joining our mailing list. You will now receive email updates about Rinker Hall.");
+}
+// Clear confirmation message
+function clearConf() {
+     alert("This will clear all of your information. Are you sure you would like to proceed?");
+}
